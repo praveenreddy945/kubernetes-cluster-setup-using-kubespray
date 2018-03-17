b9719bcc3c3f26b1c1a5c382d5410736026215c7
mjindal <jindal.manishkumar1@gmail.com>
2018-03-17 07:30:10 +0000

    Added install Script

---
+++ install.sh
@@ -0,0 +1,43 @@
+yum install -y gcc openssl-devel bzip2-devel
+
+cd /usr/src
+
+yum install -y wget
+
+wget https://www.python.org/ftp/python/3.6.4/Python-3.6.4.tgz
+
+tar xzf Python-3.6.4.tgz
+
+cd Python-3.6.4
+
+./configure --enable-optimizations
+
+make altinstall
+
+rm -y /usr/src/Python-3.6.4.tgz
+
+cd /usr/local/bin/
+
+./pip3.6 install virtualenv
+
+./virtualenv -p /usr/local/bin/python3.6 /python3-kubernetes
+
+cd /python3-kubernetes/bin
+
+source activate
+
+cd /kubernetes 
+
+pip3.6 install -r requirements.txt
+
+yum install -y http://opensource.wandisco.com/centos/6/git/x86_64/wandisco-git-release-6-1.noarch.rpm
+
+yum install -y git
+
+git clone https://github.com/manishjindal/kubespray.git
+
+yes | cp -f hosts.ini kubespray/inventory/mycluster/hosts.ini
+
+cd kubespray
+
+ansible-playbook -i inventory/mycluster/hosts.ini cluster.yml
